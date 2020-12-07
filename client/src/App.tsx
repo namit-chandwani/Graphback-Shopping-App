3fafdb1f8c2b333996bd1b31731a8c3d85f3fc70
Namit Chandwani <namitchandwani99@gmail.com>
2020-12-08 05:09:29 +0530

    Add routing to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Cart from "./components/Cart/Cart";
+import Products from "./components/Product/Products";
+import AddProduct from "./components/Product/AddProduct";
+import Orders from "./components/Order/Orders";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to App.js</h1>
-    </div>
+    <BrowserRouter>
+      <h1>Welcome to App.tsx</h1>
+      <Switch>
+        <Redirect exact from="/" to="/products" />
+        <Route path="/products" component={Products} />
+        <Route path="/add-product" component={AddProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/orders" component={Orders} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
